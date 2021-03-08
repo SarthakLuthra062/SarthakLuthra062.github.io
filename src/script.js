c99ef5a6a227659e0819fac953635b6ccab8cba8
Sarthak Luthra <57385445+SarthakLuthra062@users.noreply.github.com>
2021-03-08 09:14:05 +0530

    Minor Update

--- src/script.js
+++ src/script.js
@@ -1,6 +1,5 @@
 //Import Statements
 import * as THREE from "../three/build/three.module.js";
-//import * as dat from "dat.gui";
 import { VRButton } from "../three/examples/jsm/webxr/VRButton.js";
 import { OrbitControls } from "../three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "../three/examples/jsm/loaders/GLTFLoader.js";
@@ -16,13 +15,6 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 10000);
 camera.position.set(-1.716,-0.5,20.779);
 camera.rotation.set(-11,-0.7,0);
-
-//Character Initialisation
-let model,neck,waist,possibleAnims,mixer,idle,clips;
-let clock = new THREE.Clock();
-let currentlyAnimating = false;
-let raycaster = new THREE.Raycaster();
-let loaderAnim = document.getElementById('js-loader');
 
 //Scene Loader
 const loader = new GLTFLoader();
@@ -63,67 +55,9 @@ var pointLight = new THREE.PointLight(0xffffff, 0.5);
 pointLight.position.set(25, 50, 25);
 scene.add(pointLight);
 
-//Character Loader
-loader.load("../static/model/Stacy.glb",function(Model){
-    model = Model.scene;
-
-    //Texture and Material
-    let characterTexture = new THREE.TextureLoader().load("../static/model/stacy.jpg");
-    characterTexture.flipY = false;
-    const stacy_mtl = new THREE.MeshPhongMaterial({
-        map: characterTexture,
-        color: 0xffffff,
-        skinning: true
-      });
-
-    let fileAnimations = Model.animations;
-
-      model.traverse(o => {
-      if (o.isMesh) {
-        o.castShadow = true;
-        o.receiveShadow = true;
-        o.material = stacy_mtl;
-      }
-      // Reference the neck and waist bones
-      if (o.isBone && o.name === 'mixamorigNeck') { 
-        neck = o;
-      }
-      if (o.isBone && o.name === 'mixamorigSpine') { 
-        waist = o;
-      }
-    });
-    
-    model.scale.set(3.5,3.5,3.5);
-    model.position.set(-4.5,-5,2.572)
-    scene.add(model);
-
-    //Anims
-    loaderAnim.remove();
-    mixer = new THREE.AnimationMixer(model);
-    clips = fileAnimations.filter(val => val.name !== 'idle');
-    let idleAnim = THREE.AnimationClip.findByName(fileAnimations, 'idle');
-    idleAnim.tracks.splice(3, 3);
-    idleAnim.tracks.splice(9, 3);
-    idle = mixer.clipAction(idleAnim);
-    idle.play();
-    possibleAnims = clips.map(val => {
-    let clip = THREE.AnimationClip.findByName(clips, val.name);
-    clip.tracks.splice(3, 3);
-    clip.tracks.splice(9, 3);
-    clip = mixer.clipAction(clip);
-    return clip;
-       }
-      );
-  },
-  undefined,
-  function(error) {
-    console.error(error);
-  }
-);
-
-
 //Video Player
 const video = document.getElementById("video");
+video.src = [ 'https://www.youtube.com/embed/', '2d9oB0vaQQ4', '?rel=0' ].join( '' );
 video.crossOrigin = 'anonymous';
 video.addEventListener( 'play', function () {
 this.currentTime = 3;
@@ -229,11 +163,11 @@ function cube_move()
         for ( let i = 0; i < cube_count; i ++ ) 
         {
             videoCubeMesh = meshes[ i ];
-            videoCubeMesh.rotation.x += 10 * videoCubeMesh.dx;
+            /*videoCubeMesh.rotation.x += 10 * videoCubeMesh.dx;
             videoCubeMesh.rotation.y += 10 * videoCubeMesh.dy;
             videoCubeMesh.position.x -= 10 * videoCubeMesh.dx;
             videoCubeMesh.position.y += 10 * videoCubeMesh.dy;
-            videoCubeMesh.position.z += 48 * videoCubeMesh.dx;
+            videoCubeMesh.position.z += 48 * videoCubeMesh.dx;*/
         }
     }
     if ( counter % 1000 === 0 ) 
@@ -247,106 +181,6 @@ function cube_move()
     }
     counter ++;
 }
-
-//Get Mouse Degrees and Cursor Movement
-document.addEventListener("mousemove", function(e) {
-    var mousecoords = getMousePos(e);
-      if (neck && waist) {
-        moveJoint(mousecoords, neck, 50);
-        moveJoint(mousecoords, waist, 30);
-      }
-  });
-
-function getMousePos(e) {
-    return { x: e.clientX, y: e.clientY };
-}
-  
-function moveJoint(mouse, joint, degreeLimit) {
-    let degrees = getMouseDegrees(mouse.x, mouse.y, degreeLimit);
-    joint.rotation.y = THREE.Math.degToRad(degrees.x);
-    joint.rotation.x = THREE.Math.degToRad(degrees.y);
-}
-
-function getMouseDegrees(x, y, degreeLimit) {
-    let dx = 0,
-        dy = 0,
-        xdiff,
-        xPercentage,
-        ydiff,
-        yPercentage;
-
-    let w = { x: window.innerWidth, y: window.innerHeight };
-    if (x <= w.x / 2) {
-      xdiff = w.x / 2 - x;
-      xPercentage = (xdiff / (w.x / 2)) * 100;
-      dx = ((degreeLimit * xPercentage) / 100) * -1; 
-    }
-    
-    if (x >= w.x / 2) {
-      xdiff = x - w.x / 2;
-      xPercentage = (xdiff / (w.x / 2)) * 100;
-      dx = (degreeLimit * xPercentage) / 100;
-    }
-
-    if (y <= w.y / 2) {
-      ydiff = w.y / 2 - y;
-      yPercentage = (ydiff / (w.y / 2)) * 100;
-      dy = (((degreeLimit * 0.5) * yPercentage) / 100) * -1;
-    }
-
-    if (y >= w.y / 2) {
-      ydiff = y - w.y / 2;
-      yPercentage = (ydiff / (w.y / 2)) * 100;
-      dy = (degreeLimit * yPercentage) / 100;
-    }
-    return { x: dx, y: dy };
-  } 
-
-//Click to Change Anim
-window.addEventListener("click", e => raycast(e));
-window.addEventListener("touchend", e => raycast(e, true));
-
-function raycast(e, touch = false) {
-  var mouse = {};
-  if (touch) {
-    mouse.x = 2 * (e.changedTouches[0].clientX / window.innerWidth) - 1;
-    mouse.y = 1 - 2 * (e.changedTouches[0].clientY / window.innerHeight);
-  } else {
-    mouse.x = 2 * (e.clientX / window.innerWidth) - 1;
-    mouse.y = 1 - 2 * (e.clientY / window.innerHeight);
-  }
-  raycaster.setFromCamera(mouse, camera);
-  var intersects = raycaster.intersectObjects(scene.children, true);
-
-  if (intersects[0]) {
-    var object = intersects[0].object;
-
-    if (object.name === 'stacy') {
-
-      if (!currentlyAnimating) {
-        currentlyAnimating = true;
-        playOnClick();
-      }
-    }
-  }
-}
-
-function playOnClick() {
-    let anim = Math.floor(Math.random() * possibleAnims.length) + 0;
-    playModifierAnimation(idle, 0.25, possibleAnims[anim], 0.25);
-}
-
-function playModifierAnimation(from, fSpeed, to, tSpeed) {
-    to.setLoop(THREE.LoopOnce);
-    to.reset();
-    to.play();
-    from.crossFadeTo(to, fSpeed, true);
-    setTimeout(function() {
-      from.enabled = true;
-      to.crossFadeTo(from, tSpeed, true);
-      currentlyAnimating = false;
-    }, to._clip.duration * 1000 - ((tSpeed + fSpeed) * 1000));
-} 
 
 //Update Function
 function update()
